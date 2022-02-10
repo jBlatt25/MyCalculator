8bc1b75e50555e6270ccd1ea1562a9f7a37bc2b2
blattj <blattj@wit.edu>
2022-02-10 13:55:02 -0500

    Initial commit

---
+++ app/src/main/java/wit/edu/moblieapp/mycalculator/Calculator.java
@@ -0,0 +1,50 @@
+package wit.edu.moblieapp.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+        Button activity1_btn = (Button)findViewById(R.id.calbutton);
+        activity1_btn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(Calculator.this, Result.class);
+                Log.v("MyApp","displaying result");
+                startActivity(intent);
+
+            }
+        });
+        }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioFemale:
+                if (checked)
+
+                    break;
+            case R.id.radioMale:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+}
---
+++ app/src/main/java/wit/edu/moblieapp/mycalculator/Result.java
@@ -0,0 +1,19 @@
+package wit.edu.moblieapp.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+
+
+
+
+    }
+}
